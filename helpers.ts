ac6c4c107c1c68070459fa1b9f9b6d02469860f7
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 13:30:34 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T10:28:07.387Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T10:30:34.603Z
+Commit: Fix memory leak in event handlers
