2745b0fa0772f192adebad4968ac8f2d45cbcdd1
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 12:33:03 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:24:38.311Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-09T09:33:03.542Z
+Commit: Fix memory leak in event handlers
