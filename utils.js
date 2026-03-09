8b2a2fe9629531f1d3e9565c10577ce8cfa3c446
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 13:30:44 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T10:30:40.779Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T10:30:45.289Z
+Commit: Fix memory leak in event handlers
