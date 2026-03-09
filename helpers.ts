f2d7f7f6ca70ef47ca52589d8e5a28f42035837a
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 13:30:37 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T10:30:34.603Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-09T10:30:38.311Z
+Commit: Resolve timezone issues in date handling
