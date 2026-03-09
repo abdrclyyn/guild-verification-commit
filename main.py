d8f9c14aab96dadb4eac81faa89be1e40bf3f415
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 12:33:10 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:33:04.830Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-09T09:33:10.948Z
+Commit: Fix CSS styling bug on mobile devices
