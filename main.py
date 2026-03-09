06b42a0f23ea7bd36057a869f1f909415d1d82ae
abdrclyyn <abdurrahmanceylans16@gmail.com>
2026-03-09 12:56:43 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:39:33.980Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-09T09:56:43.935Z
+Commit: Introduce real-time notifications
